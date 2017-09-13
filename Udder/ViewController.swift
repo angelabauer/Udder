b156bc1b3365dd07e4582f8e96b26ce7c57b683d
Angela Yu <angela_yu@hotmail.co.uk>
2017-09-13 16:19:10 +0100

    Add view will appear

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -19,6 +19,10 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        //
+    }
 
 
 }
